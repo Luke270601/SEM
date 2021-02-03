567422c06d86265c273afdbbac2af943b04f2077
Luke270601 <40437228@live.napier.ac.uk>
2021-02-03 12:51:44 +0000

    Updated app to interact with mongoDB

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,32 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        //run locally
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Connect to MongoDB
+        // MongoClient mongoClient = new MongoClient("mongo-dbserver");
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("myDB");
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+        // Create a document to store
+        Document doc = new Document("Name", "Cameron Hunt")
+                .append("Class", "Software Engineering Methods")
+                .append("Year", "2021")
+                .append("Result", new Document("CW", 100).append("Exam", 100));
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
